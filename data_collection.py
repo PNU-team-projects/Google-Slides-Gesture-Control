be22d43faff15fda408e9728df02e8dd1ef1b4c5
VladHolobyn <vlad.holobyn@gmail.com>
2024-06-12 08:05:35 +0300

    Added dataset and scripts for detection and data creation

---
+++ data_collection.py
@@ -0,0 +1,62 @@
+import cv2
+import numpy as np
+import math
+from cvzone.HandTrackingModule import HandDetector
+
+
+offset = 30
+img_size = 300
+
+
+cap = cv2.VideoCapture(0)
+detector = HandDetector(maxHands=1)
+
+
+SIGN = "--ENTER-SIGN--"
+counter = 0
+
+
+while True:
+    success, img = cap.read()
+    hands, imgWithDetection = detector.findHands(img, draw=False)
+
+    if hands: 
+        hand = hands[0]
+        x,y,w,h = hand['bbox']
+
+        imgCrop = img[y-offset : y+h+offset, x-offset : x+w+offset]
+        
+        if imgCrop is not None:
+            finalImage = np.ones((img_size, img_size, 3), np.uint8)*255
+
+            aspectRation = h/w
+            if aspectRation > 1:
+                k= img_size/h
+                wCal = math.floor(k*w)
+                wGap = math.floor((img_size-wCal)/2)
+
+                img_resized = cv2.resize(imgCrop, (wCal, img_size))
+                finalImage[:, wGap: wCal+wGap] = img_resized
+            else:
+                k= img_size/w
+                hCal = math.floor(k*h)
+                hGap = math.floor((img_size-hCal)/2)
+
+                img_resized = cv2.resize(imgCrop, (img_size, hCal))
+                finalImage[hGap: hCal+hGap, :] = img_resized
+
+            try:
+                cv2.imshow("imgCrop", imgCrop)
+                cv2.imshow("finalImage", finalImage)
+            except:
+                print("error")
+
+    cv2.imshow("Image", imgWithDetection)
+    key = cv2.waitKey(1)
+
+    if key == ord("s"):
+        counter +=1
+        cv2.imwrite(f'data/{SIGN}/{SIGN}_{counter}.jpg', finalImage)
+        print(counter)
+    elif key == 27: #ESC
+        break
---
+++ main.py
@@ -0,0 +1,71 @@
+import cv2
+import numpy as np
+import math
+from cvzone.HandTrackingModule import HandDetector
+from tensorflow.keras import models
+
+
+labels = ["A", "B", "C", "F", "G", "L", "P", "Q", "R", "Y"]
+offset = 30
+img_size = 300
+
+
+# Loading the model from files
+json_file = open('saved_models/model (1).json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = models.model_from_json(loaded_model_json)
+
+loaded_model.built = True
+
+loaded_model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+loaded_model.load_weights("saved_models/model_weights (1).h5")
+# Model is loaded 
+
+cap = cv2.VideoCapture(0)
+detector = HandDetector(maxHands=1)
+
+
+
+while True:
+    success, img = cap.read()
+    hands, imgWithDetection = detector.findHands(img, draw=False)
+
+    if hands: 
+        hand = hands[0]
+        x,y,w,h = hand['bbox']
+
+        imgCrop = img[y-offset : y+h+offset, x-offset : x+w+offset]
+        finalImage = np.ones((img_size, img_size, 3), np.uint8)*255
+
+        aspectRation = h/w
+        if aspectRation > 1:
+            k = img_size/h
+            wCal = math.floor(k*w)
+            wGap = math.floor((img_size-wCal)/2)
+
+            img_resized = cv2.resize(imgCrop, (wCal, img_size))
+            finalImage[:, wGap : wCal+wGap] = img_resized
+        else:
+            k = img_size/w
+            hCal = math.floor(k*h)
+            hGap = math.floor((img_size-hCal)/2)
+
+            img_resized = cv2.resize(imgCrop, (img_size, hCal))
+            finalImage[hGap : hCal+hGap, :] = img_resized
+
+        try:
+            prediction = loaded_model.predict(np.array([finalImage/255]))
+            label = np.argmax(prediction[0])
+            print(labels[label])
+
+            cv2.putText(imgWithDetection,  labels[label], (10,450), 1, 3, (0, 255, 0), 2, cv2.LINE_AA)
+            cv2.imshow("finalImage", finalImage)
+        except:
+            print("error")
+
+    cv2.imshow("Image", imgWithDetection)
+    
+    key = cv2.waitKey(3)
+    if key == 27: #ESC
+        break
